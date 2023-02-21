Fix bug in code causing script to yield incorrect results

cssfinder/algorithm/backend/numpy/_impl/_complex128.py
@@ -29,11 +29,6 @@ import numpy as np
 import numpy.typing as npt
 from numba import jit
 
-# def create_rho_1(prefix, rho, mode, d1, vis):
-#     rhoa = np.zeros(rho.shape, dtype=np.complex128)
-#     np.fill_diagonal(rhoa, rho.diagonal())
-#     return rhoa
-
 
 @jit(nopython=True, nogil=True, cache=True)
 def product(
@@ -52,10 +47,10 @@ def normalize(mtx: npt.NDArray[np.complex128]) -> npt.NDArray[np.complex128]:
 
 
 @jit(nopython=True, nogil=True, cache=True)
-def get_random_haar(size: int) -> npt.NDArray[np.complex128]:
+def get_random_haar(depth: int) -> npt.NDArray[np.complex128]:
     """Generate a random vector with Haar measure."""
-    real = np.random.normal(0, 1, size)
-    imaginary = np.random.normal(0, 1, size)
+    real = np.random.normal(0, 1, depth)
+    imaginary = np.random.normal(0, 1, depth)
     return real + 1j * imaginary
 
 
@@ -66,12 +61,12 @@ def project(mtx1: npt.NDArray[np.complex128]) -> npt.NDArray[np.complex128]:
 
 
 @jit(nopython=True, nogil=True, cache=True)
-def random_d_fs(size: int, sub_sys_size: int) -> npt.NDArray[np.complex128]:
+def random_d_fs(depth: int, quantity: int) -> npt.NDArray[np.complex128]:
     """Random n quDit state."""
-    vector = normalize(get_random_haar(size))
+    vector = normalize(get_random_haar(depth))
 
-    for _ in range(sub_sys_size - 1):
-        idx_vector = normalize(get_random_haar(size))
+    for _ in range(quantity - 1):
+        idx_vector = normalize(get_random_haar(depth))
         vector = np.outer(vector, idx_vector).flatten()
 
     vector = project(vector)
@@ -83,8 +78,8 @@ def random_d_fs(size: int, sub_sys_size: int) -> npt.NDArray[np.complex128]:
 def optimize_d_fs(
     rho2: npt.NDArray[np.complex128],
     rho3: npt.NDArray[np.complex128],
-    size: int,
-    sub_sys_size: int,
+    depth: int,
+    quantity: int,
     epochs: int,
 ) -> npt.NDArray[np.complex128]:
     """Optimize implementation for FSnQd mode."""
@@ -92,12 +87,12 @@ def optimize_d_fs(
     product_2_3 = product(rho2, rho3)
 
     # To make sure rotated_2 is not unbound
-    unitary = random_unitary_d_fs(size, sub_sys_size, 0)
+    unitary = random_unitary_d_fs(depth, quantity, 0)
     rotated_2 = rotate(rho2, unitary)
 
     for idx in range(epochs):
-        idx_mod = idx % int(sub_sys_size)
-        unitary = random_unitary_d_fs(size, sub_sys_size, idx_mod)
+        idx_mod = idx % int(quantity)
+        unitary = random_unitary_d_fs(depth, quantity, idx_mod)
         rotated_2 = rotate(rho2, unitary)
 
         product_rot2_3 = product(rotated_2, rho3)
@@ -117,22 +112,22 @@ def optimize_d_fs(
 # @jit(nopython=True, nogil=True, cache=True)
 # @jit(forceobj=True)
 def random_unitary_d_fs(
-    size: int, sub_sys_size: int, idx: int
+    depth: int, quantity: int, idx: int
 ) -> npt.NDArray[np.complex128]:
     """N quDits."""
-    value = _random_unitary_d_fs_val(size)
-    mtx = expand_d_fs(value, size, sub_sys_size, idx)
+    value = _random_unitary_d_fs_val(depth)
+    mtx = expand_d_fs(value, depth, quantity, idx)
     return mtx
 
 
 @jit(nopython=True, nogil=True, cache=True)
-def _random_unitary_d_fs_val(size: int) -> npt.NDArray[np.complex128]:
+def _random_unitary_d_fs_val(depth: int) -> npt.NDArray[np.complex128]:
     real = np.cos(0.01 * np.pi)
     imag = 1j * np.sin(0.01 * np.pi)
     value = real + imag - 1
 
-    random_mtx = random_d_fs(size, 1)
-    identity_mtx = np.identity(size).astype(np.complex128)
+    random_mtx = random_d_fs(depth, 1)
+    identity_mtx = np.identity(depth).astype(np.complex128)
     value = np.add(np.multiply(value, random_mtx), identity_mtx)
     return value
 
@@ -141,16 +136,16 @@ def _random_unitary_d_fs_val(size: int) -> npt.NDArray[np.complex128]:
 # @jit(forceobj=True)
 def expand_d_fs(
     value: npt.NDArray[np.complex128],
-    size: int,
-    sub_sys_size: int,
+    depth: int,
+    quantity: int,
     idx: int,
 ) -> npt.NDArray[np.complex128]:
     """Expand an operator to n quDits."""
-    size_1 = int(size**idx)
-    identity_1 = np.identity(size_1).astype(np.complex128)
+    depth_1 = int(depth**idx)
+    identity_1 = np.identity(depth_1).astype(np.complex128)
 
-    size_2 = int(size ** (sub_sys_size - idx - 1))
-    identity_2 = np.identity(size_2).astype(np.complex128)
+    depth_2 = int(depth ** (quantity - idx - 1))
+    identity_2 = np.identity(depth_2).astype(np.complex128)
 
     kronecker_1 = kronecker(identity_1, value)
     kronecker_2 = kronecker(kronecker_1, identity_2)

cssfinder/algorithm/backend/numpy/base.py
@@ -43,8 +43,9 @@ class NumPyBase(BackendBase):
 
     def __init__(self, initial: State, mode: AlgoMode, visibility: float) -> None:
         super().__init__(initial, mode, visibility)
-        self._visibility = self._create_visibility_matrix(initial)
-        self._intermediate = self.initial.state.copy()
+
+        self._visibility = self._create_visibility_matrix()
+        self._intermediate = self._create_intermediate_state()
         self._corrections = []
 
         self._aa4 = 2 * self.impl.product(self._visibility, self._intermediate)
@@ -52,20 +53,27 @@ class NumPyBase(BackendBase):
         self._visibility_reduced = self._visibility - self._intermediate
         self._dd1 = self.impl.product(self._intermediate, self._visibility_reduced)
 
-    def _create_visibility_matrix(self, initial: State) -> npt.NDArray[np.complex128]:
-        return self.visibility * initial.state + (1 - self.visibility) * np.identity(
-            len(initial.state), dtype=np.complex128
+    def _create_visibility_matrix(self) -> npt.NDArray[np.complex128]:
+        vis_state = self.visibility * self.initial.state
+        inv_vis_ident = (1 - self.visibility) * np.identity(
+            len(self.initial.state), dtype=np.complex128
         )
+        return vis_state + inv_vis_ident / len(self.initial.state)
+
+    def _create_intermediate_state(self) -> npt.NDArray[np.complex128]:
+        intermediate = np.zeros_like(self._visibility, dtype=np.complex128)
+        np.fill_diagonal(intermediate, self._visibility.diagonal())
+        return intermediate
 
     @property
     def state(self) -> npt.NDArray[np.complex128]:
         """Return current system state with all optimizations applied."""
-        return self._intermediate
+        return self._intermediate.copy()
 
     @property
     def corrections(self) -> list[tuple[int, int, float]]:
         """Return list of all corrections found during optimization."""
-        return self._corrections
+        return self._corrections.copy()
 
     @property
     def corrections_count(self) -> int:
@@ -76,8 +84,6 @@ class NumPyBase(BackendBase):
         """Run sequence of iterations without stopping to check any stop conditions."""
 
         depth = self.initial.depth
-        quantity = self.initial.quantity
-
         quantity = self.initial.quantity
         epochs = 20 * depth * depth * quantity
 
@@ -111,11 +117,11 @@ class NumPyBase(BackendBase):
         aa2 = 2 * self.impl.product(self._visibility, alternative_state)
         aa5 = 2 * self.impl.product(self._intermediate, alternative_state)
 
-        coef = -(-self._aa4 + aa2 + aa5 - 2 * aa3) / (2 * (self._aa6 - aa5 + aa3))
+        param = -(-self._aa4 + aa2 + aa5 - 2 * aa3) / (2 * (self._aa6 - aa5 + aa3))
 
-        if 0 <= coef <= 1:
+        if 0 <= param <= 1:
             self._intermediate = (
-                coef * self._intermediate + (1 - coef) * alternative_state
+                param * self._intermediate + (1 - param) * alternative_state
             )
 
             self._visibility_reduced = self._visibility - self._intermediate

cssfinder/algorithm/backend/numpy/impl.py
@@ -45,7 +45,7 @@ class Implementation(Protocol):
         ...
 
     @staticmethod
-    def get_random_haar(size: int) -> npt.NDArray[np.complex128]:
+    def get_random_haar(depth: int) -> npt.NDArray[np.complex128]:
         """Generate a random vector with Haar measure."""
         ...
 
@@ -55,7 +55,7 @@ class Implementation(Protocol):
         ...
 
     @staticmethod
-    def random_d_fs(size: int, sub_sys_size: int) -> npt.NDArray[np.complex128]:
+    def random_d_fs(depth: int, quantity: int) -> npt.NDArray[np.complex128]:
         """Random n quDit state."""
         ...
 
@@ -63,8 +63,8 @@ class Implementation(Protocol):
     def optimize_d_fs(
         rho2: npt.NDArray[np.complex128],
         rho3: npt.NDArray[np.complex128],
-        size: int,
-        sub_sys_size: int,
+        depth: int,
+        quantity: int,
         epochs: int,
     ) -> npt.NDArray[np.complex128]:
         """Optimize implementation for FSnQd mode."""
@@ -72,7 +72,7 @@ class Implementation(Protocol):
 
     @staticmethod
     def random_unitary_d_fs(
-        size: int, sub_sys_size: int, idx: int
+        depth: int, quantity: int, idx: int
     ) -> npt.NDArray[np.complex128]:
         """N quDits."""
         ...
@@ -80,8 +80,8 @@ class Implementation(Protocol):
     @staticmethod
     def expand_d_fs(  # pylint: disable=invalid-name
         value: npt.NDArray[np.complex128],
-        size: int,
-        sub_sys_size: int,
+        depth: int,
+        quantity: int,
         idx: int,
     ) -> npt.NDArray[np.complex128]:
         """Expand an operator to n quDits."""

cssfinder/algorithm/gilbert.py
@@ -23,9 +23,14 @@
 
 from __future__ import annotations
 
+import logging
+from typing import Optional
+
+import numpy as np
+import numpy.typing as npt
+
 from cssfinder.algorithm import backend as _backend
 from cssfinder.io.asset_loader import State
-from cssfinder.log import get_logger
 from cssfinder.project.cssfproject import AlgoMode, Backend, Precision
 
 
@@ -40,39 +45,70 @@ class Gilbert:
         precision: Precision,
         visibility: float,
     ) -> None:
-        self.logger = get_logger()
         self.initial = initial
         self.mode = mode
         self.precision = precision
         self.visibility = visibility
+
         backend_type = _backend.new(backend, self.precision)
         self.backend = backend_type(self.initial, self.mode, self.visibility)
+
+        self._state: Optional[npt.NDArray[np.complex128]] = None
+        self._corrections: Optional[list[tuple[int, int, float]]] = None
 
     def run(self, epochs: int, iterations: int, max_corrections: int) -> None:
         """Run epochs of iterations each, or up to max_corrections found."""
         total_iterations = epochs * iterations
 
         for epoch_index in range(epochs):
-            self.logger.info(
-                "Executing epoch {0} / {1} ({2:.2%})",
+            logging.info(
+                "Executing epoch %r / %r (%.2f)",
                 epoch_index + 1,
                 epochs,
-                (epoch_index + 1) / epochs,
+                ((epoch_index + 1) / epochs) * 100,
             )
             # Run N iterations of algorithm without checking stop conditions.
             self.backend.run_epoch(iterations, epoch_index)
 
             iterations_executed = (epoch_index + 1) * iterations
-            self.logger.debug(
-                "Executed {0} iterations, total {1} / {2} ({3:.2%})",
+            logging.debug(
+                "Executed %r iterations, total %r / %r (%.2f)",
                 iterations,
                 iterations_executed,
                 total_iterations,
-                iterations_executed / total_iterations,
+                (iterations_executed / total_iterations) * 100,
             )
             # Check if we already reached expected number of corrections
             if self.backend.corrections_count >= max_corrections:
-                self.logger.info(
-                    "Reached expected maximal number of corrections {}", max_corrections
+                logging.info(
+                    "Reached expected maximal number of corrections %r", max_corrections
                 )
                 break
+
+        self._state = self.backend.state
+        self._corrections = self.backend.corrections
+
+    @property
+    def state(self) -> npt.NDArray[np.complex128]:
+        """Returns correction from saturated algorithm."""
+        if self._state is None:
+            raise AlgorithmNotSaturatedError("Run algorithm first to obtain state!")
+        return self._state
+
+    @property
+    def corrections(self) -> list[tuple[int, int, float]]:
+        """Returns correction from saturated algorithm."""
+        if self._corrections is None:
+            raise AlgorithmNotSaturatedError(
+                "Run algorithm first to obtain corrections!"
+            )
+        return self._corrections
+
+
+class AlgorithmError(Exception):
+    """Base for exceptions raised by gilbert algorithm."""
+
+
+class AlgorithmNotSaturatedError(Exception):
+    """Raised when action was performed on which required algorithm to finish execution
+    on instance which was not run."""

cssfinder/io/asset_loader.py
@@ -24,6 +24,8 @@ compatible with CSSFProject in version 1.0.0."""
 
 from __future__ import annotations
 
+import json
+import logging
 import math
 import operator
 from dataclasses import dataclass
@@ -34,7 +36,6 @@ import numpy.typing as npt
 
 from cssfinder.constants import PRIMES
 from cssfinder.io.matrix import MatrixIO
-from cssfinder.log import get_logger
 from cssfinder.project.cssfproject import CSSFProject
 
 
@@ -46,7 +47,6 @@ class AssetLoader:
 
     def load_initial_state(self, force_squash: bool = False) -> State:
         """Load initial state from file indicated by cssfproject configuration."""
-        logger = get_logger()
 
         state_props = self.project.resources.initial_state
         # Replace magic variables with dynamically determined values
@@ -54,8 +54,8 @@ class AssetLoader:
 
         loader = MatrixIO.new(state_matrix_file_path)
         mtx = loader.load().astype(np.complex128)
-        logger.info(
-            "Loaded matrix from {0!r} of shape {1}", state_matrix_file_path, mtx.shape
+        logging.info(
+            "Loaded matrix from %r of shape %r", state_matrix_file_path, mtx.shape
         )
 
         # Forcefully reshape array to match shape criterion
@@ -79,10 +79,10 @@ class AssetLoader:
             depth, quantity = self.detect_depth_and_quantity(total_size)
 
             if old_quantity is not None:
-                logger.warning(
+                logging.warning(
                     (
-                        "State quantity ({0}) was ignored, as depth is None, "
-                        + "quantity of {1} was deduced."
+                        "State quantity (%r) was ignored, as depth is None, "
+                        + "quantity of %r was deduced."
                     ),
                     old_quantity,
                     quantity,
@@ -91,32 +91,31 @@ class AssetLoader:
         elif quantity is None:
             quantity = self.detect_system_quantity(depth, total_size)
 
-        logger.info(
-            "Matrix represents system with depth = {0}, quantity = {1}", depth, quantity
+        logging.info(
+            "Matrix represents system with depth = %r, quantity = %r", depth, quantity
         )
         return State(state=mtx, depth=depth, quantity=quantity)
 
     def _check_matrix_shape(self, mtx: npt.NDArray[np.complex128]) -> None:
         """Check if ndarray conforms shape rules."""
-        logger = get_logger()
 
         if len(mtx.shape) == 2:
             pass
 
         elif len(mtx.shape) > 2:
-            logger.critical(
-                "Expected square matrix but got tensor with shape {0}", mtx.shape
+            logging.critical(
+                "Expected square matrix but got tensor with shape %r", mtx.shape
             )
             raise NotExpectedTensor(mtx)
 
         elif len(mtx.shape) == 1:
-            logger.critical(
-                "Expected square matrix but got vector with shape {0}", mtx.shape
+            logging.critical(
+                "Expected square matrix but got vector with shape %r", mtx.shape
             )
             raise NotExpectedVector(mtx)
 
         elif len(mtx.shape) == 0:
-            logger.critical("Expected square matrix but got scalar ({0})", mtx)
+            logging.critical("Expected square matrix but got scalar (%r)", mtx)
             raise NotExpectedScalar(mtx)
 
         else:
@@ -125,7 +124,7 @@ class AssetLoader:
         # Only square matrices are accepted.
         x_size, y_size = mtx.shape
         if x_size != y_size:
-            logger.critical("Expected square matrix, but received shape {0}", mtx.shape)
+            logging.critical("Expected square matrix, but received shape %r", mtx.shape)
             raise IncorrectMatrixShape(mtx)
 
     def detect_depth_and_quantity(self, total: int) -> tuple[int, int]:
@@ -150,8 +149,8 @@ class AssetLoader:
             quantity = int(math.log(total, depth))
 
             if quantity == int(quantity):
-                get_logger().debug(
-                    "Deduced quantity {0} and depth {1} when given total size {2}",
+                logging.debug(
+                    "Deduced quantity %r and depth %r when given total size %r",
                     depth,
                     quantity,
                     total,
@@ -185,9 +184,7 @@ class AssetLoader:
         quantity = int(math.log(total, depth))
 
         if quantity == int(quantity):
-            get_logger().debug(
-                "Deduced quantity {0} when given depth of {1}", depth, quantity
-            )
+            logging.debug("Deduced quantity %r when given depth of %r", depth, quantity)
             return quantity
 
         raise ValueError(
@@ -200,6 +197,44 @@ class AssetLoader:
 
     def load_projection(self) -> None:
         """Load matrix describing projection of system state."""
+
+    def save_output_state(self, state: npt.NDArray[np.complex128]) -> None:
+        """Save state in project output state file `state.mtx` in `output` directory of
+        your project.
+
+        Parameters
+        ----------
+        state : npt.NDArray[np.complex128]
+            State matrix to save.
+        """
+        dest = self.project.output / "state.mtx"
+        logging.debug(
+            "Saving output state to %r with matrix size %r",
+            dest.as_posix(),
+            state.shape,
+        )
+        mtx_io = MatrixIO.new(dest)
+        mtx_io.dump(state)
+
+    def save_output_corrections(
+        self, corrections: list[tuple[int, int, float]]
+    ) -> None:
+        """Save corrections in project output state file `corrections.json` in `output`
+        directory of your project.
+
+        Parameters
+        ----------
+        corrections : npt.NDArray[np.complex128]
+            State matrix to save.
+        """
+        dest = self.project.output / "corrections.json"
+        logging.debug(
+            "Saving output state to %r with total corrections %r",
+            dest.as_posix(),
+            len(corrections),
+        )
+        with dest.open("w", encoding="utf-8") as file:
+            json.dump(corrections, file, indent=2)
 
 
 @dataclass

cssfinder/io/matrix.py
@@ -23,6 +23,7 @@
 
 from __future__ import annotations
 
+import logging
 from abc import ABC, abstractmethod
 from pathlib import Path
 from typing import Optional, Type
@@ -31,7 +32,7 @@ import numpy as np
 import numpy.typing as npt
 import scipy.io
 
-from cssfinder.log import get_logger
+import cssfinder
 
 
 class MatrixIO(ABC):
@@ -63,9 +64,8 @@ class MatrixIO(ABC):
         if file_format is None:
             file_format = file_path.suffix
 
-        logger = get_logger()
-        logger.debug(
-            "Using file format {!r} for file {}", file_format, file_path.as_posix()
+        logging.debug(
+            "Using file format %r for file %r", file_format, file_path.as_posix()
         )
 
         return FORMAT_TO_LOADER[file_format](file_path)
@@ -73,6 +73,10 @@ class MatrixIO(ABC):
     @abstractmethod
     def load(self) -> npt.NDArray[np.int64 | np.float64 | np.complex128]:
         """Load matrix from file as numpy array."""
+
+    @abstractmethod
+    def dump(self, data: npt.NDArray[np.int64 | np.float64 | np.complex128]) -> None:
+        """Dump matrix to file from numpy array."""
 
 
 class MatrixMarketIO(MatrixIO):
@@ -82,6 +86,14 @@ class MatrixMarketIO(MatrixIO):
         mtx = scipy.io.mmread(self.file_path.as_posix())
         assert mtx is not None
         return np.array(mtx)
+
+    def dump(self, data: npt.NDArray[np.int64 | np.float64 | np.complex128]) -> None:
+        self.file_path.touch(0o664, exist_ok=True)
+        scipy.io.mmwrite(
+            self.file_path.as_posix(),
+            data,
+            comment=f"Created with CSSFinder {cssfinder.__version__}.",
+        )
 
 
 FORMAT_TO_LOADER: dict[str, Type[MatrixIO]] = {